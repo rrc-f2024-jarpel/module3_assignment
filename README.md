d5ea06a6e83945bfffe6de5a09c5dcd5c2a18efa
jpelletier5@rrc.ca <jaredwpelletier@gmail.com>
2024-10-02 09:59:22 -0500

    Read data from account_balances.txt to dict in interest_update.py

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ Create a virtual ATM for PiXELL River customers to interact with
 ## bank_transactions.py
     This is a program so that customers can easily
     manage their bank transactions, such as withdrawl and deposit
+
+## interest_update.py
+    Allows VP to apply interest to multiple accounts based on their current balance
--- interest_update.py
+++ interest_update.py
@@ -6,5 +6,14 @@ Author:
 Date:
     Created On; 2024-10-01 14:46
 """
+#This is for debuggig purposes, remove later if needed
+from pprint import pprint
 
 
+accountBalances = {}
+with open("account_balances.txt", "r") as accountDetails:
+    for line in accountDetails:
+        key, value = line.strip().split("|")
+        accountBalances[key] = float(value)
+
+pprint(accountBalances)

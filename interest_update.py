4986855709e22e5bc7cfbc3f4e2a528f6384f833
jpelletier5@rrc.ca <jaredwpelletier@gmail.com>
2024-10-03 15:45:49 -0500

    Data is now read from the CSV file and displayed to console

--- interest_update.py
+++ interest_update.py
@@ -17,8 +17,9 @@ with open("account_balances.txt", "r") as accountDetails:
         key, value = line.strip().split("|")
         accountBalances[key] = float(value)
 #Test print to see if it is reading correct values
+print("----------Read from account_balances.txt----------")
 pprint(accountBalances)
-
+print("--------------------------------------------------")
 #Iterate through lines and modify their values according to their coresponding rates
 for line in accountBalances:
     if accountBalances[line] >= 5000: #Interest rate of 5.0
@@ -30,11 +31,22 @@ for line in accountBalances:
     elif accountBalances[line] < 0: #Interest rate of 10
         accountBalances[line] = accountBalances[line] + ((accountBalances[line] * 0.1) /12)
 #Test print to see if the values have beed modified
+print("--------Modify account balances and display-------")
 pprint(accountBalances)
+print("--------------------------------------------------")
 
+#Creates(If not created) and writes to updated_balances_JP.csv
 fieldNames = ["Account", "Balance"]
 with open("updated_balances_JP.csv", "w", newline='') as file:
     writer = csv.DictWriter(file, fieldnames=fieldNames) #Set header
     writer.writeheader()
     for account, balance in accountBalances.items(): #Write to the csv file
         writer.writerow({'Account': account, 'Balance': balance})
+
+#Reads from the previously created CSV file and prints to console
+print("------Display From updated_balances_JP.csv--------")
+with open("updated_balances_JP.csv", "r")as file:
+    reader = csv.DictReader(file)
+    for line in reader:
+        print(line['Account'], line['Balance'])
+print("--------------------------------------------------")
